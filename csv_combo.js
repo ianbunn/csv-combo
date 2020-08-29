49800a1bb4fcfbe15597430680bb78881b7f9a76
ianbunn <ianrichardbunn@gmail.com>
2020-08-29 14:41:23 -0500

    Switch to OOP, update console.log to .info or .error

--- csv_combo.js
+++ csv_combo.js
@@ -8,8 +8,9 @@ const newCSVFileNameIndex = inputFilePaths.findIndex(f => f === '#') + 1
 const newCSVFileName = newCSVFileNameIndex ? inputFilePaths.splice(newCSVFileNameIndex - 1, 2)[1] : 'combined_files'
 const validatedNewCSVFileName = newCSVFileName.endsWith('.csv') ? newCSVFileName : `${newCSVFileName}.csv`
 
-const csvCombo = {
-    readCSVFile: (file) => {
+class CSV {
+
+    static readCSVFile(file) {
         const fileExt = file.substring(file.lastIndexOf('.'))
         return new Promise((resolve, reject) => {
             if (fileExt === '.csv') {
@@ -24,20 +25,21 @@ const csvCombo = {
                         resolve(readData)
                     })
             } else {
-                console.log(`${file} is not a CSV. Only CSV files allowed. Please try again.`)
+                console.error(`${file} is not a CSV. Only CSV files allowed. Please try again.`)
             }
         })
-    },
-    mergeFiles: async (files, combinedFile) => {
+    }
+
+    static async mergeFiles(files, combinedFile) {
         if (files.length) {
-            const promises = files.map(async (file) => await csvCombo.readCSVFile(file))
+            const promises = files.map(async (file) => await CSV.readCSVFile(file))
             const results = await Promise.all(promises)
             const formatCsv = fastCSV.format({headers: true, quote: null})
-            const writedata = fs.createWriteStream(combinedFile)
-            writedata.on('finish', () => {
-                console.log(`Successfully combined files to ${validatedNewCSVFileName}!`)
+            const writeData = fs.createWriteStream(combinedFile)
+            writeData.on('finish', () => {
+                console.info(`Successfully combined files to ${validatedNewCSVFileName}!`)
             })
-            formatCsv.pipe(writedata)
+            formatCsv.pipe(writeData)
             results.forEach((result) => {
                 result.forEach((data) => {
                     formatCsv.write(data)
@@ -45,15 +47,15 @@ const csvCombo = {
             })
             formatCsv.end()
         } else {
-            console.log('No files to merge, please enter the paths to merge the files')
+            console.error('No files to merge, please enter the paths to merge the files')
             return files
         }
-    },
+    }
 }
 
-csvCombo.mergeFiles(inputFilePaths, validatedNewCSVFileName)
+CSV.mergeFiles(inputFilePaths, validatedNewCSVFileName)
 
 module.exports = {
-    readCSVFile: csvCombo.readCSVFile,
-    mergeFiles: csvCombo.mergeFiles
+    readCSVFile: CSV.readCSVFile,
+    mergeFiles: CSV.mergeFiles
 }
